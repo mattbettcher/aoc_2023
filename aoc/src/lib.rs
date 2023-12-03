3c729824d10d443ff9ad82bcdb699ed8f96d1bc4
mattbettcher <mattbettcher@gmail.com>
2023-12-02 22:20:28 -0500

    Reorganized a little

---
+++ aoc/src/lib.rs
@@ -0,0 +1,44 @@
+
+pub enum Part {
+    One,
+    Two,
+}
+
+pub trait Runner {
+    fn year_and_day(&self) -> (usize, usize);
+    fn part1_description(&self) -> &str;
+    fn part2_description(&self) -> &str;
+
+    fn part1(&mut self, input: &str) -> i32;
+    fn part2(&mut self, input: &str) -> i32;
+    
+    fn test(&mut self, part: Part, test_input: &str, answer: i32) {
+        let (res, part_str) = match part {
+            Part::One => (self.part1(test_input), "Part 1"),
+            Part::Two => (self.part2(test_input), "Part 2"),
+        };
+        if res == answer {
+            println!("{part_str} --- Correct answer found: {answer}");
+        } else {
+            println!("{part_str} --- Correct answer is: {answer}, found {res}");
+        }
+    }
+
+    fn run(&mut self, part: Part, input: &str) -> i32 {
+        let (res, part_str) = match part {
+            Part::One => (self.part1(input), "Part 1"),
+            Part::Two => (self.part2(input), "Part 2"),
+        };
+        println!("{part_str} --- Answer found: {res}");
+        res
+    }
+
+    fn run_both_parts(&mut self, input: &str) -> (i32, i32) {
+        let (part1_res, part2_res) = (self.part1(input), self.part2(input));
+
+        println!("Part 1 --- Answer found: {part1_res}");
+        println!("Part 2 --- Answer found: {part2_res}");
+
+        (part1_res, part2_res)
+    }
+}
--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,8 +1,44 @@
+use aoc::{Part, Runner};
+
 fn main() {
     let input = include_str!("../input");
-    let answer = process_part_2(input);
-    println!("{answer}");
+
+    let mut day2 = Day2 {};
+
+    day2.run_both_parts(input);
 }
+
+#[test]
+fn test_part1() {
+    let mut day2 = Day2 {};
+
+    day2.test(
+        Part::One,
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        8,
+    );
+}
+
+#[test]
+fn test_part2() {
+    let mut day2 = Day2 {};
+
+    day2.test(
+        Part::Two,
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        2286,
+    );
+}
+
+pub struct Day2;
 
 #[derive(Debug)]
 struct Game {
@@ -17,143 +53,187 @@ enum Color {
     Green(u32),
 }
 
-fn process_part_1(input: &str) -> i32 {
-    let mut games = vec![];
-    let output: Vec<_> = input
-        .lines()
-        .map(|line| {
-            let sub_line = line[5..].split_terminator(':').collect::<Vec<&str>>();
-            let id: i32 = sub_line.first().unwrap().parse::<i32>().unwrap();
+impl Runner for Day2 {
+    fn year_and_day(&self) -> (usize, usize) {
+        (2023, 2)
+    }
 
-            let handfulls: Vec<Color> = vec![];
-            let colors: Vec<Vec<Color>> = sub_line[1]
-                .split_terminator(';')
-                .map(|l| {
-                    let mut colors = vec![];
-                    l.split(',').for_each(|mut handfull| {
-                        handfull = handfull.trim(); // remove leading ws
-                        let handfull = handfull.split_whitespace().collect::<Vec<&str>>();
-                        let count = handfull[0].parse::<u32>().unwrap();
-                        let color = match handfull[1] {
-                            "red" => Color::Red(count),
-                            "green" => Color::Green(count),
-                            "blue" => Color::Blue(count),
-                            _ => panic!(),
-                        };
-                        colors.push(color);
-                    });
-                    colors
-                }).collect();
+    fn part1(&mut self, input: &str) -> i32 {
+        let mut games = vec![];
+        let output: Vec<_> = input
+            .lines()
+            .map(|line| {
+                let sub_line = line[5..].split_terminator(':').collect::<Vec<&str>>();
+                let id: i32 = sub_line.first().unwrap().parse::<i32>().unwrap();
 
-            let mut game = Game {
-                id,
-                handfulls: colors,
-            };
-            games.push(game);
-        })
-        .collect();
-    //dbg!(games);
+                let handfulls: Vec<Color> = vec![];
+                let colors: Vec<Vec<Color>> = sub_line[1]
+                    .split_terminator(';')
+                    .map(|l| {
+                        let mut colors = vec![];
+                        l.split(',').for_each(|mut handfull| {
+                            handfull = handfull.trim(); // remove leading ws
+                            let handfull = handfull.split_whitespace().collect::<Vec<&str>>();
+                            let count = handfull[0].parse::<u32>().unwrap();
+                            let color = match handfull[1] {
+                                "red" => Color::Red(count),
+                                "green" => Color::Green(count),
+                                "blue" => Color::Blue(count),
+                                _ => panic!(),
+                            };
+                            colors.push(color);
+                        });
+                        colors
+                    })
+                    .collect();
 
-    let mut possible_games = 0;
+                let mut game = Game {
+                    id,
+                    handfulls: colors,
+                };
+                games.push(game);
+            })
+            .collect();
+        //dbg!(games);
 
-    for game in games {
-        let mut imposible = false;
-        for handfull in game.handfulls {
-            for color in handfull {
-                match color {
-                    Color::Red(r) => if r > 12 { imposible = true },
-                    Color::Green(g) => if g > 13 { imposible = true },
-                    Color::Blue(b) => if b > 14 { imposible = true },
+        let mut possible_games = 0;
+
+        for game in games {
+            let mut imposible = false;
+            for handfull in game.handfulls {
+                for color in handfull {
+                    match color {
+                        Color::Red(r) => {
+                            if r > 12 {
+                                imposible = true
+                            }
+                        }
+                        Color::Green(g) => {
+                            if g > 13 {
+                                imposible = true
+                            }
+                        }
+                        Color::Blue(b) => {
+                            if b > 14 {
+                                imposible = true
+                            }
+                        }
+                    }
                 }
             }
+            if !imposible {
+                possible_games += game.id
+            }
         }
-        if !imposible {
-            possible_games += game.id
-        }
+        possible_games
     }
-    possible_games
-}
 
-#[test]
-fn part_1_test() {
-    let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    fn part2(&mut self, input: &str) -> i32 {
+        let mut games = vec![];
+        let output: Vec<_> = input
+            .lines()
+            .map(|line| {
+                let sub_line = line[5..].split_terminator(':').collect::<Vec<&str>>();
+                let id: i32 = sub_line.first().unwrap().parse::<i32>().unwrap();
+
+                let handfulls: Vec<Color> = vec![];
+                let colors: Vec<Vec<Color>> = sub_line[1]
+                    .split_terminator(';')
+                    .map(|l| {
+                        let mut colors = vec![];
+                        l.split(',').for_each(|mut handfull| {
+                            handfull = handfull.trim(); // remove leading ws
+                            let handfull = handfull.split_whitespace().collect::<Vec<&str>>();
+                            let count = handfull[0].parse::<u32>().unwrap();
+                            let color = match handfull[1] {
+                                "red" => Color::Red(count),
+                                "green" => Color::Green(count),
+                                "blue" => Color::Blue(count),
+                                _ => panic!(),
+                            };
+                            colors.push(color);
+                        });
+                        colors
+                    })
+                    .collect();
+
+                let mut game = Game {
+                    id,
+                    handfulls: colors,
+                };
+                games.push(game);
+            })
+            .collect();
+        //dbg!(games);
+
+        let mut min_cubes = 0;
+
+        for game in games {
+            let mut min_red = 0;
+            let mut min_blue = 0;
+            let mut min_green = 0;
+            for handfull in game.handfulls {
+                for color in handfull {
+                    match color {
+                        Color::Red(r) => min_red = min_red.max(r),
+                        Color::Green(g) => min_green = min_green.max(g),
+                        Color::Blue(b) => min_blue = min_blue.max(b),
+                    }
+                }
+            }
+            min_cubes += (min_red * min_blue * min_green) as i32;
+        }
+        min_cubes
+    }
+
+    fn part1_description(&self) -> &str {
+        r#"--- Day 2: Cube Conundrum ---
+You're launched high into the atmosphere! The apex of your trajectory just barely reaches the surface of a large island floating in the sky. You gently land in a fluffy pile of leaves. It's quite cold, but you don't see much snow. An Elf runs over to greet you.
+
+The Elf explains that you've arrived at Snow Island and apologizes for the lack of snow. He'll be happy to explain the situation, but it's a bit of a walk, so you have some time. They don't get many visitors up here; would you like to play a game in the meantime?
+
+As you walk, the Elf shows you a small bag and some cubes which are either red, green, or blue. Each time you play this game, he will hide a secret number of cubes of each color in the bag, and your goal is to figure out information about the number of cubes.
+
+To get information, once a bag has been loaded with cubes, the Elf will reach into the bag, grab a handful of random cubes, show them to you, and then put them back in the bag. He'll do this a few times per game.
+
+You play several games and record the information from each game (your puzzle input). Each game is listed with its ID number (like the 11 in Game 11: ...) followed by a semicolon-separated list of subsets of cubes that were revealed from the bag (like 3 red, 5 green, 4 blue).
+
+For example, the record of a few games might look like this:
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+In game 1, three sets of cubes are revealed from the bag (and then put back again). The first set is 3 blue cubes and 4 red cubes; the second set is 1 red cube, 2 green cubes, and 6 blue cubes; the third set is only 2 green cubes.
 
-    let val = process_part_1(input);
+The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
 
-    assert_eq!(8, val);
-}
+In the example above, games 1, 2, and 5 would have been possible if the bag had been loaded with that configuration. However, game 3 would have been impossible because at one point the Elf showed you 20 red cubes at once; similarly, game 4 would also have been impossible because the Elf showed you 15 blue cubes at once. If you add up the IDs of the games that would have been possible, you get 8.
 
-
-fn process_part_2(input: &str) -> i32 {
-    let mut games = vec![];
-    let output: Vec<_> = input
-        .lines()
-        .map(|line| {
-            let sub_line = line[5..].split_terminator(':').collect::<Vec<&str>>();
-            let id: i32 = sub_line.first().unwrap().parse::<i32>().unwrap();
-
-            let handfulls: Vec<Color> = vec![];
-            let colors: Vec<Vec<Color>> = sub_line[1]
-                .split_terminator(';')
-                .map(|l| {
-                    let mut colors = vec![];
-                    l.split(',').for_each(|mut handfull| {
-                        handfull = handfull.trim(); // remove leading ws
-                        let handfull = handfull.split_whitespace().collect::<Vec<&str>>();
-                        let count = handfull[0].parse::<u32>().unwrap();
-                        let color = match handfull[1] {
-                            "red" => Color::Red(count),
-                            "green" => Color::Green(count),
-                            "blue" => Color::Blue(count),
-                            _ => panic!(),
-                        };
-                        colors.push(color);
-                    });
-                    colors
-                }).collect();
-
-            let mut game = Game {
-                id,
-                handfulls: colors,
-            };
-            games.push(game);
-        })
-        .collect();
-    //dbg!(games);
-
-    let mut min_cubes = 0;
-
-    for game in games {
-        let mut min_red = 0;
-        let mut min_blue = 0;
-        let mut min_green = 0;
-        for handfull in game.handfulls {
-            for color in handfull {
-                match color {
-                    Color::Red(r) => min_red = min_red.max(r),
-                    Color::Green(g) => min_green = min_green.max(g),
-                    Color::Blue(b) => min_blue = min_blue.max(b),
-                }
-            }
-        }
-        min_cubes += (min_red * min_blue * min_green) as i32;
+Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?"#
     }
-    min_cubes
-}
 
-#[test]
-fn part_2_test() {
-    let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    fn part2_description(&self) -> &str {
+        r#"--- Part Two ---
+The Elf says they've stopped producing snow because they aren't getting any water! He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
+
+As you continue your walk, the Elf poses a second question: in each game you played, what is the fewest number of cubes of each color that could have been in the bag to make the game possible?
+
+Again consider the example games from earlier:
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If any color had even one fewer cube, the game would have been impossible.
+Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
 
-    let val = process_part_2(input);
-
-    assert_eq!(2286, val);
+For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?"#
+    }
 }
