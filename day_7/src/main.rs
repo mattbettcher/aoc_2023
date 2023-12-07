18b7094461ca67d0c2382cd150754eb3990dd61b
mattbettcher <mattbettcher@gmail.com>
2023-12-07 17:56:06 -0500

    Fixed one sort and was able to greatly reduce extra matchs

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -41,22 +41,6 @@ QQQJA 483",
 }
 
 pub struct Day7;
-//#[derive(Debug, Copy, Clone, Ord, PartialEq, PartialOrd, Eq, Hash)]
-//pub enum Card {
-//    Two = 2,
-//    Three = 3,
-//    Four = 4,
-//    Five = 5,
-//    Six = 6,
-//    Seven = 7,
-//    Eight = 8,
-//    Nine = 9,
-//    Ten = 10,
-//    Jack = 11,
-//    Queen = 12,
-//    King = 13,
-//    Ace = 14,
-//}
 
 #[derive(Debug, Copy, Clone, Ord, PartialEq, PartialOrd, Eq, Hash)]
 pub enum Card {
@@ -159,7 +143,7 @@ impl Hand {
         }
 
         let mut card_counts = map.clone().into_iter().collect::<Vec<(Card, usize)>>();
-        card_counts.sort_unstable();
+        card_counts.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
 
         // if we have one or more jokers
         if map.contains_key(&Card::Joker) {
@@ -167,14 +151,14 @@ impl Hand {
             map.remove(&Card::Joker);
             // need to update card_counts
             card_counts = map.clone().into_iter().collect::<Vec<(Card, usize)>>();
-            card_counts.sort_by(|a,b| a.1.partial_cmp(&b.1).unwrap());
+            card_counts.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
             self.remove_jokers();
             match (jokers, map.len()) {
                 // only found one type of card in the hand and they were all jokers
                 (_, 0) => {
                     self.adj_cards = vec![Card::Ace, Card::Ace, Card::Ace, Card::Ace, Card::Ace]
                 }
-                // only one other card and 4 jacks
+                // only one other card and 4 jokers
                 (_, 1) => {
                     let c = self.cards[0];
                     self.adj_cards = vec![c, c, c, c, c]
@@ -212,13 +196,19 @@ impl Hand {
                 // found 3 types of cards in the hand + a joker
                 // 1 + 1 + 2 + J
                 (_, 3) => match &card_counts[..] {
-                    [(first_single, 1), (second_single, 1), (pair, 2)] 
+                    [(first_single, 1), (second_single, 1), (pair, 2)]
                     | [(pair, 2), (first_single, 1), (second_single, 1)]
-                    | [(first_single, 1) ,(pair, 2),  (second_single, 1)] => {
+                    | [(first_single, 1), (pair, 2), (second_single, 1)] => {
                         self.adj_cards = vec![*pair, *pair, *pair, *first_single, *second_single]
                     }
                     [(first_single, 1), (second_single, 1), (third_single, 1)] => {
-                        self.adj_cards = vec![*first_single, *first_single, *first_single, *second_single, *third_single]
+                        self.adj_cards = vec![
+                            *first_single,
+                            *first_single,
+                            *first_single,
+                            *second_single,
+                            *third_single,
+                        ]
                     }
                     _ => unreachable!(),
                 },
@@ -245,7 +235,7 @@ impl Hand {
                 }
             }
             card_counts = map.clone().into_iter().collect::<Vec<(Card, usize)>>();
-            card_counts.sort_by(|a,b| a.1.partial_cmp(&b.1).unwrap());
+            card_counts.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
         }
 
         match map.len() {
@@ -254,15 +244,15 @@ impl Hand {
             // found two types of cards in the hand
             // either 4 + 1 or 3 + 2
             2 => match &card_counts[..] {
-                [(_, 1), (_, 4)] | [(_, 4), (_, 1)] => self.kind = HandType::FourOfAKind,
-                [(_, 2), (_, 3)] | [(_, 3), (_, 2)] => self.kind = HandType::FullHouse,
+                [(_, 1), (_, 4)] => self.kind = HandType::FourOfAKind,
+                [(_, 2), (_, 3)] => self.kind = HandType::FullHouse,
                 _ => unreachable!(),
             },
             // found 3 types of cards in the hand
             // either 2 + 2 + 1 or 3 + 1 + 1
             3 => match &card_counts[..] {
-                [(_, 1), (_, 2), (_, 2)] | [(_, 2), (_, 2), (_, 1)] | [(_, 2), (_, 1), (_, 2)] => self.kind = HandType::TwoPair,
-                [(_, 1), (_, 1), (_, 3)] | [(_, 3), (_, 1), (_, 1)] | [(_, 1), (_, 3), (_, 1)] => self.kind = HandType::ThreeOfAKind,
+                [(_, 1), (_, 2), (_, 2)] => self.kind = HandType::TwoPair,
+                [(_, 1), (_, 1), (_, 3)] => self.kind = HandType::ThreeOfAKind,
                 _ => unreachable!(),
             },
             // found 4 types of cards in the hand
