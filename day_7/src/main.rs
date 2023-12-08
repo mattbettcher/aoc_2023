afa8127b2f9202e144aa12686e7cf1a7d4793193
mattbettcher <mattbettcher@gmail.com>
2023-12-08 17:11:44 -0500

    Day 7 part 2 refactor - not working

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     let input = include_str!("../input");
 
     let mut day7 = Day7 {};
-
-    day7.run(Part::Two, input);
+    //day7.run(Part::One, input);   // 250347426
+    day7.run(Part::Two, input);     // 251224870
 }
 
 #[test]
@@ -130,7 +130,7 @@ impl Hand {
     }
 
     pub fn compute_with_joker(&mut self) {
-        let mut map: HashMap<Card, usize> = HashMap::with_capacity(63);
+        let mut map: HashMap<Card, usize> = HashMap::with_capacity(7);
 
         for c in &self.cards {
             if map.contains_key(c) {
@@ -141,126 +141,102 @@ impl Hand {
                 map.insert(*c, 1);
             }
         }
+        let has_joker = map.contains_key(&Card::Joker);
+        // if we have one or more jokers
+        self.remove_jokers();
+        if has_joker {
+            map.remove(&Card::Joker);
+        }
 
         let mut card_counts = map.clone().into_iter().collect::<Vec<(Card, usize)>>();
         card_counts.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
 
-        // if we have one or more jokers
-        if map.contains_key(&Card::Joker) {
-            let jokers = *map.get(&Card::Joker).unwrap();
-            map.remove(&Card::Joker);
-            // need to update card_counts
-            card_counts = map.clone().into_iter().collect::<Vec<(Card, usize)>>();
-            card_counts.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
-            self.remove_jokers();
-            match (jokers, map.len()) {
-                // only found one type of card in the hand and they were all jokers
-                (_, 0) => {
-                    self.adj_cards = vec![Card::Ace, Card::Ace, Card::Ace, Card::Ace, Card::Ace]
+        match (has_joker, map.len()) {
+            // all jokers
+            (true, 0) => {
+                self.adj_cards = vec![Card::Ace, Card::Ace, Card::Ace, Card::Ace, Card::Ace];
+                self.kind = HandType::FiveOfAKind;
+            }
+            (false, 1) => self.kind = HandType::FiveOfAKind,
+            (true, 1) => {
+                let c = self.adj_cards[0];
+                self.adj_cards = vec![c, c, c, c, c];
+                self.kind = HandType::FiveOfAKind;
+            }
+            (false, 2) => {
+                match &card_counts.iter().map(|(_, i)| *i).collect::<Vec<usize>>()[..] {
+                    [1, 4] => self.kind = HandType::FourOfAKind,
+                    [2, 3] => self.kind = HandType::FullHouse,
+                    _ => unreachable!(),
                 }
-                // only one other card and 4 jokers
-                (_, 1) => {
-                    let c = self.cards[0];
-                    self.adj_cards = vec![c, c, c, c, c]
-                }
-                // found two types of cards in the hand + a joker
-                // either 3 + 1 or 2 + 2
-                // should be one joker?
-                (_, 2) => {
-                    match &card_counts[..] {
-                        [(single_card, 1), (pair, 2)] => {
-                            self.adj_cards = vec![*single_card, *pair, *pair, *pair, *pair]
-                        }
-                        [(single_card, 1), (tok, 3)] => {
-                            self.adj_cards = vec![*single_card, *tok, *tok, *tok, *tok]
-                        }
-                        [(pair1, 2), (pair2, 2)] => {
-                            // need to determine the higher of the 2 pairs
-                            if pair1 > pair2 {
-                                self.adj_cards = vec![*pair1, *pair1, *pair1, *pair2, *pair2]
-                            } else {
-                                self.adj_cards = vec![*pair2, *pair2, *pair2, *pair1, *pair1]
-                            }
-                        }
-                        [(first, 1), (second, 1)] => {
-                            // need to determine the higher of the 2 pairs
-                            if first > second {
-                                self.adj_cards = vec![*first, *first, *first, *first, *second]
-                            } else {
-                                self.adj_cards = vec![*second, *second, *second, *second, *first]
-                            }
-                        }
-                        _ => unreachable!(),
+            }
+            (true, 2) => {
+                match &card_counts[..] {
+                    [(first, 1), (pair, 2)] => {
+                        self.adj_cards = vec![*first, *pair, *pair, *pair, *pair];
+                        self.kind = HandType::FourOfAKind;
                     }
-                }
-                // found 3 types of cards in the hand + a joker
-                // 1 + 1 + 2 + J
-                (_, 3) => match &card_counts[..] {
-                    [(first_single, 1), (second_single, 1), (pair, 2)]
-                    | [(pair, 2), (first_single, 1), (second_single, 1)]
-                    | [(first_single, 1), (pair, 2), (second_single, 1)] => {
-                        self.adj_cards = vec![*pair, *pair, *pair, *first_single, *second_single]
+                    [(first, 1), (tok, 3)] => {
+                        self.adj_cards = vec![*first, *tok, *tok, *tok, *tok];
+                        self.kind = HandType::FourOfAKind;
                     }
-                    [(first_single, 1), (second_single, 1), (third_single, 1)] => {
-                        self.adj_cards = vec![
-                            *first_single,
-                            *first_single,
-                            *first_single,
-                            *second_single,
-                            *third_single,
-                        ]
+                    [(pair1, 2), (pair2, 2)] => {
+                        // need to determine the higher of the 2 pairs
+                        if pair1 > pair2 {
+                            self.adj_cards = vec![*pair1, *pair1, *pair1, *pair2, *pair2];
+                            self.kind = HandType::ThreeOfAKind;
+                        } else {
+                            self.adj_cards = vec![*pair2, *pair2, *pair2, *pair1, *pair1];
+                            self.kind = HandType::ThreeOfAKind;
+                        }
+                    }
+                    [(first, 1), (second, 1)] => {
+                        // need to determine the higher of the 2 cards
+                        if first > second {
+                            self.adj_cards = vec![*first, *first, *first, *first, *second];
+                            self.kind = HandType::FourOfAKind;
+                        } else {
+                            self.adj_cards = vec![*second, *second, *second, *second, *first];
+                            self.kind = HandType::FourOfAKind;
+                        }
                     }
                     _ => unreachable!(),
-                },
-                // found 4 types of cards in the hand
-                // 1 + 1 + 1 + 1 + J
-                (_, 4) => {
-                    // get highest card
-                    let mut cc = self.cards.clone();
-                    cc.sort();
-                    let hc = cc[0];
-                    self.adj_cards = vec![hc, hc, cc[1], cc[2], cc[3]];
                 }
-                _ => unreachable!(),
             }
-            // be sure to reset the map and card_counts if we had jokers!
-            map.clear();
-            for c in &self.adj_cards {
-                if map.contains_key(c) {
-                    if let Some(v) = map.get_mut(c) {
-                        *v += 1;
+            (false, 3) => match &card_counts.iter().map(|(_, i)| *i).collect::<Vec<usize>>()[..] {
+                [1, 2, 2] => self.kind = HandType::TwoPair,
+                [1, 1, 3] => self.kind = HandType::ThreeOfAKind,
+                _ => unreachable!(),
+            },
+            (true, 3) => match &card_counts[..] {
+                [(first, 1), (second, 1), (pair, 2)] => {
+                    self.adj_cards = vec![*pair, *pair, *pair, *first, *second];
+                    self.kind = HandType::ThreeOfAKind;
+                }
+                [(first, 1), (sec, 1), (third, 1)] => {
+                    // only one where high card isn't correct
+                    if first > sec && first > third {
+                        self.adj_cards = vec![*first, *first, *first, *sec, *third];
+                    } else if sec > first && sec > third {
+                        self.adj_cards = vec![*sec, *sec, *sec, *first, *third];
+                    } else {
+                        self.adj_cards = vec![*third, *third, *third, *first, *sec];
                     }
-                } else {
-                    map.insert(*c, 1);
+                    self.kind = HandType::ThreeOfAKind;
                 }
+                _ => unreachable!(),
+            },
+            (false, 4) => self.kind = HandType::OnePair,
+            (true, 4) => {
+                // get highest card
+                let mut cc = self.cards.clone();
+                cc.sort();
+                cc.reverse();
+                let hc = cc[0];
+                self.adj_cards = vec![hc, hc, cc[1], cc[2], cc[3]];
+                self.kind = HandType::OnePair;
             }
-            card_counts = map.clone().into_iter().collect::<Vec<(Card, usize)>>();
-            card_counts.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
-        }
-
-        match map.len() {
-            // only found one type of card in the hand
-            1 => self.kind = HandType::FiveOfAKind,
-            // found two types of cards in the hand
-            // either 4 + 1 or 3 + 2
-            2 => match &card_counts[..] {
-                [(_, 1), (_, 4)] => self.kind = HandType::FourOfAKind,
-                [(_, 2), (_, 3)] => self.kind = HandType::FullHouse,
-                _ => unreachable!(),
-            },
-            // found 3 types of cards in the hand
-            // either 2 + 2 + 1 or 3 + 1 + 1
-            3 => match &card_counts[..] {
-                [(_, 1), (_, 2), (_, 2)] => self.kind = HandType::TwoPair,
-                [(_, 1), (_, 1), (_, 3)] => self.kind = HandType::ThreeOfAKind,
-                _ => unreachable!(),
-            },
-            // found 4 types of cards in the hand
-            // 2 + 1 + 1 + 1
-            4 => self.kind = HandType::OnePair,
-            // found 5 types of cards in the hand
-            // 1 + 1 + 1 + 1 + 1
-            5 => self.kind = HandType::HighCard,
+            (false, 5) => self.kind = HandType::HighCard,
             _ => unreachable!(),
         }
     }
@@ -309,7 +285,7 @@ impl Runner for Day7 {
         }
 
         for hand in &mut hands {
-            hand.compute();
+            hand.compute_with_joker();
         }
         // all hands now have their type computed
         // now we sort by type
@@ -325,7 +301,6 @@ impl Runner for Day7 {
                 map.insert(hand.kind, vec![hand]);
             }
         }
-        //dbg!(&map);
         // now we sort types by card values
         for (_kind, hands) in map.iter_mut() {
             hands.sort_by(|a, b| {
@@ -341,7 +316,6 @@ impl Runner for Day7 {
                 std::cmp::Ordering::Equal
             })
         }
-        //dbg!(&map);
         // build full list of hands
         let mut all_hands = map.values().into_iter().flatten().collect::<Vec<&&Hand>>();
         all_hands.sort_by(|a, b| a.kind.partial_cmp(&b.kind).unwrap());
@@ -391,7 +365,7 @@ impl Runner for Day7 {
         // all hands now have their type computed
         // now we sort by type
         hands.sort_by(|a, b| a.kind.partial_cmp(&b.kind).unwrap());
-        //dbg!(&hands);
+
         let mut map: HashMap<HandType, Vec<&Hand>> = HashMap::with_capacity(8);
         for hand in &hands {
             if map.contains_key(&hand.kind) {
@@ -402,7 +376,6 @@ impl Runner for Day7 {
                 map.insert(hand.kind, vec![hand]);
             }
         }
-        //dbg!(&map);
         // now we sort types by card values
         for (_kind, hands) in map.iter_mut() {
             hands.sort_by(|a, b| {
@@ -418,7 +391,6 @@ impl Runner for Day7 {
                 std::cmp::Ordering::Equal
             })
         }
-        //dbg!(&map);
         // build full list of hands
         let mut all_hands = map.values().into_iter().flatten().collect::<Vec<&&Hand>>();
         all_hands.sort_by(|a, b| a.kind.partial_cmp(&b.kind).unwrap());
