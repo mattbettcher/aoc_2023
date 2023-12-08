aa8ee97c68731ef03dc8d7ed2fc8ef940ec5de5c
mattbettcher <mattbettcher@gmail.com>
2023-12-08 18:08:04 -0500

    Vastly simplified, probably faster?

--- day_7/src/main.rs
+++ day_7/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let input = include_str!("../input");
 
     let mut day7 = Day7 {};
-    //day7.run(Part::One, input);   // 250347426
+    day7.run(Part::One, input);   // 250347426
     day7.run(Part::Two, input);     // 251224870
 }
 
@@ -74,16 +74,14 @@ pub enum HandType {
 
 #[derive(Debug, Clone)]
 pub struct Hand {
-    cards: Vec<Card>,
-    adj_cards: Vec<Card>,
+    cards: [Card; 5],
     bid: usize,
     kind: HandType,
 }
 
 impl Hand {
-    pub fn compute(&mut self) {
-        let mut map: HashMap<Card, usize> = HashMap::with_capacity(12);
-
+    fn count_cards(&self) -> (bool, Vec<usize>, usize) {
+        let mut map: HashMap<Card, usize> = HashMap::with_capacity(7);
         for c in &self.cards {
             if map.contains_key(c) {
                 if let Some(v) = map.get_mut(c) {
@@ -93,159 +91,45 @@ impl Hand {
                 map.insert(*c, 1);
             }
         }
-        let mut card_counts = map.clone().into_values().collect::<Vec<usize>>();
-        card_counts.sort_unstable();
-
-        match map.len() {
-            // only found one type of card in the hand
-            1 => self.kind = HandType::FiveOfAKind,
-            // found two types of cards in the hand
-            // either 4 + 1 or 3 + 2
-            2 => {
-                debug_assert!(card_counts.len() == 2);
-                match &card_counts[..] {
-                    [1, 4] => self.kind = HandType::FourOfAKind,
-                    [2, 3] => self.kind = HandType::FullHouse,
-                    _ => unreachable!(),
-                }
-            }
-            // found 3 types of cards in the hand
-            // either 2 + 2 + 1 or 3 + 1 + 1
-            3 => {
-                debug_assert!(card_counts.len() == 3);
-                match &card_counts[..] {
-                    [1, 2, 2] => self.kind = HandType::TwoPair,
-                    [1, 1, 3] => self.kind = HandType::ThreeOfAKind,
-                    _ => unreachable!(),
-                }
-            }
-            // found 4 types of cards in the hand
-            // 2 + 1 + 1 + 1
-            4 => self.kind = HandType::OnePair,
-            // found 5 types of cards in the hand
-            // 1 + 1 + 1 + 1 + 1
-            5 => self.kind = HandType::HighCard,
-            _ => unreachable!(),
-        }
+        let joker = map.contains_key(&Card::Joker);
+        if joker { map.remove(&Card::Joker); }
+        let mut cc = map.values().map(|v|*v).collect::<Vec<usize>>();
+        cc.sort_by(|a, b| a.partial_cmp(&b).unwrap());
+        (joker, cc, map.len())
     }
 
     pub fn compute_with_joker(&mut self) {
-        let mut map: HashMap<Card, usize> = HashMap::with_capacity(7);
-
-        for c in &self.cards {
-            if map.contains_key(c) {
-                if let Some(v) = map.get_mut(c) {
-                    *v += 1;
-                }
-            } else {
-                map.insert(*c, 1);
-            }
-        }
-        let has_joker = map.contains_key(&Card::Joker);
-        // if we have one or more jokers
-        self.remove_jokers();
-        if has_joker {
-            map.remove(&Card::Joker);
-        }
-
-        let mut card_counts = map.clone().into_iter().collect::<Vec<(Card, usize)>>();
-        card_counts.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
-
-        match (has_joker, map.len()) {
-            // all jokers
-            (true, 0) => {
-                self.adj_cards = vec![Card::Ace, Card::Ace, Card::Ace, Card::Ace, Card::Ace];
-                self.kind = HandType::FiveOfAKind;
-            }
+        let (has_joker, cc, cards) = self.count_cards();
+        match (has_joker, cards) {
+            (true, 0) => self.kind = HandType::FiveOfAKind,
             (false, 1) => self.kind = HandType::FiveOfAKind,
-            (true, 1) => {
-                let c = self.adj_cards[0];
-                self.adj_cards = vec![c, c, c, c, c];
-                self.kind = HandType::FiveOfAKind;
-            }
+            (true, 1) => self.kind = HandType::FiveOfAKind,
             (false, 2) => {
-                match &card_counts.iter().map(|(_, i)| *i).collect::<Vec<usize>>()[..] {
+                match &cc[..] {
                     [1, 4] => self.kind = HandType::FourOfAKind,
                     [2, 3] => self.kind = HandType::FullHouse,
                     _ => unreachable!(),
                 }
             }
             (true, 2) => {
-                match &card_counts[..] {
-                    [(first, 1), (pair, 2)] => {
-                        self.adj_cards = vec![*first, *pair, *pair, *pair, *pair];
-                        self.kind = HandType::FourOfAKind;
-                    }
-                    [(first, 1), (tok, 3)] => {
-                        self.adj_cards = vec![*first, *tok, *tok, *tok, *tok];
-                        self.kind = HandType::FourOfAKind;
-                    }
-                    [(pair1, 2), (pair2, 2)] => {
-                        // need to determine the higher of the 2 pairs
-                        if pair1 > pair2 {
-                            self.adj_cards = vec![*pair1, *pair1, *pair1, *pair2, *pair2];
-                            self.kind = HandType::ThreeOfAKind;
-                        } else {
-                            self.adj_cards = vec![*pair2, *pair2, *pair2, *pair1, *pair1];
-                            self.kind = HandType::ThreeOfAKind;
-                        }
-                    }
-                    [(first, 1), (second, 1)] => {
-                        // need to determine the higher of the 2 cards
-                        if first > second {
-                            self.adj_cards = vec![*first, *first, *first, *first, *second];
-                            self.kind = HandType::FourOfAKind;
-                        } else {
-                            self.adj_cards = vec![*second, *second, *second, *second, *first];
-                            self.kind = HandType::FourOfAKind;
-                        }
-                    }
+                match &cc[..] {
+                    [1, 2] => self.kind = HandType::FourOfAKind,
+                    [1, 3] => self.kind = HandType::FourOfAKind,
+                    [2, 2] => self.kind = HandType::FullHouse,
+                    [1, 1] => self.kind = HandType::FourOfAKind,
                     _ => unreachable!(),
                 }
             }
-            (false, 3) => match &card_counts.iter().map(|(_, i)| *i).collect::<Vec<usize>>()[..] {
+            (false, 3) => match &cc[..] {
                 [1, 2, 2] => self.kind = HandType::TwoPair,
                 [1, 1, 3] => self.kind = HandType::ThreeOfAKind,
                 _ => unreachable!(),
             },
-            (true, 3) => match &card_counts[..] {
-                [(first, 1), (second, 1), (pair, 2)] => {
-                    self.adj_cards = vec![*pair, *pair, *pair, *first, *second];
-                    self.kind = HandType::ThreeOfAKind;
-                }
-                [(first, 1), (sec, 1), (third, 1)] => {
-                    // only one where high card isn't correct
-                    if first > sec && first > third {
-                        self.adj_cards = vec![*first, *first, *first, *sec, *third];
-                    } else if sec > first && sec > third {
-                        self.adj_cards = vec![*sec, *sec, *sec, *first, *third];
-                    } else {
-                        self.adj_cards = vec![*third, *third, *third, *first, *sec];
-                    }
-                    self.kind = HandType::ThreeOfAKind;
-                }
-                _ => unreachable!(),
-            },
+            (true, 3) => self.kind = HandType::ThreeOfAKind,
             (false, 4) => self.kind = HandType::OnePair,
-            (true, 4) => {
-                // get highest card
-                let mut cc = self.cards.clone();
-                cc.sort();
-                cc.reverse();
-                let hc = cc[0];
-                self.adj_cards = vec![hc, hc, cc[1], cc[2], cc[3]];
-                self.kind = HandType::OnePair;
-            }
+            (true, 4) => self.kind = HandType::OnePair,
             (false, 5) => self.kind = HandType::HighCard,
             _ => unreachable!(),
-        }
-    }
-
-    fn remove_jokers(&mut self) {
-        for i in 0..self.cards.len() {
-            if self.cards[i] != Card::Joker {
-                self.adj_cards.push(self.cards[i]);
-            }
         }
     }
 }
@@ -275,10 +159,10 @@ impl Runner for Day7 {
                     _ => panic!(),
                 })
                 .collect::<Vec<_>>();
+            // we could count the cards here and not have to keep them around at all...
             let bid = bid.parse::<usize>().unwrap();
             hands.push(Hand {
-                cards,
-                adj_cards: vec![],
+                cards: [cards[0], cards[1], cards[2], cards[3], cards[4]],
                 bid,
                 kind: HandType::Unknown,
             });
@@ -352,8 +236,7 @@ impl Runner for Day7 {
                 .collect::<Vec<_>>();
             let bid = bid.parse::<usize>().unwrap();
             hands.push(Hand {
-                cards,
-                adj_cards: vec![],
+                cards: [cards[0], cards[1], cards[2], cards[3], cards[4]],
                 bid,
                 kind: HandType::Unknown,
             });
